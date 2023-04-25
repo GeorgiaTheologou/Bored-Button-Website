ec908f82784ca0e3ba280a540823f2fa3ff480c9
GewrgiaTh <98404052+GewrgiaTh@users.noreply.github.com>
2023-04-25 17:19:50 +0100

    Tic tac toe game and js functions for red buttons

---
+++ JavaScript/scripts.js
@@ -0,0 +1,27 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const doneButton = document.getElementById("doneButton");
+doneButton.addEventListener('click', () => {
+  const selectElement = document.querySelector('.boredomLevel');
+  const selectedValue = selectElement.value;
+  alert(`${selectedValue}.`);
+});
+
+//For the red button function
+var randomlinks = [];
+
+ randomlinks[0]="TicTacToeGame.html"
+ //randomlinks[1]=""
+ //randomlinks[2]=""
+ //randomlinks[3]=""
+ 
+
+ function randomlink(){
+  window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
+}
+
