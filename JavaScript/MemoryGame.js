720da88d689e70a13b7b67f7311e8c8471962841
GewrgiaTh <98404052+GewrgiaTh@users.noreply.github.com>
2023-05-05 01:10:17 +0100

    Added some last comments in some javascript files

--- JavaScript/MemoryGame.js
+++ JavaScript/MemoryGame.js
@@ -25,6 +25,8 @@ let disableDeck = false;
 let isPlaying = false;
 let cardOne, cardTwo, timer;
 
+/*updates the timer by reducing the timeLeft variable by 1 and displays the timeLeft in the HTML document. 
+*If the timeLeft is less than or equal to zero, it clears the timer interval.*/
 function initTimer() {
     if(timeLeft <= 0) {
         return clearInterval(timer);
@@ -33,6 +35,10 @@ function initTimer() {
     timeTag.innerText = timeLeft;
 }
 
+/*Responsible for the logic of flipping cards on the game board
+ * It checks if the game is still in play, if the clicked card is not the same as the previously clicked card,
+ * if the deck is not disabled, and if time is still remaining. It flips the clicked card, increments the number of flips, 
+ * and if this is the second card being clicked, it disables the deck and matches the two cards.*/
 function flipCard({target: clickedCard}) {
     if(!isPlaying) {
         isPlaying = true;
@@ -53,6 +59,12 @@ function flipCard({target: clickedCard}) {
     }
 }
 
+/**compares the two card images for a match
+ * If they match, it increments the number of matched cards. 
+ * If all cards have been matched, it clears the timer. It removes the event listener from the two matched cards and enables the deck.
+ * If the cards do not match, it shakes the cards, 
+ * then flips them back after 1200 milliseconds, removes the shake class and re-enables the deck.
+ */
 function matchCards(img1, img2) {
     if(img1 === img2) {
         matchedCard++;
@@ -78,6 +90,11 @@ function matchCards(img1, img2) {
     }, 1200);
 }
 
+/**resets the game to its initial state by setting the timeLeft, 
+ * flips, and matchedCard variables to zero, clearing the timer interval, and re-enabling the deck. 
+ *  It shuffles the cards by creating an array of numbers representing the card image and randomly sorting them. 
+ * It then sets the card images based on their position in the array and adds a click event listener to each card.
+ */
 function shuffleCard() {
     timeLeft = maxTime;
     flips = matchedCard = 0;
--- JavaScript/TicTacToe.js
+++ JavaScript/TicTacToe.js
@@ -22,28 +22,43 @@ resultBox = document.querySelector(".result-box"),
 wonText = resultBox.querySelector(".won-text"),
 replayBtn = resultBox.querySelector("button");
 
+//To attach a click event to each box on the game board. When a box is clicked, the clickedBox() function is called.
 window.onload = ()=>{
     for (let i = 0; i < allBox.length; i++) {
        allBox[i].setAttribute("onclick", "clickedBox(this)");
     }
 }
 
+// Function to handle the click event for selectBtnX
 selectBtnX.onclick = ()=>{
+    // Add the 'hide' class to selectBox and the 'show' class to playBoard
     selectBox.classList.add("hide");
     playBoard.classList.add("show");
 }
 
+// Function to handle the click event for selectBtnO
 selectBtnO.onclick = ()=>{ 
+    // Add the 'hide' class to selectBox and the 'show' class to playBoard
     selectBox.classList.add("hide");
     playBoard.classList.add("show");
+    // Set the 'class' attribute of players to 'players active player'
     players.setAttribute("class", "players active player");
 }
+
 
 let playerXIcon = "fas fa-times",
 playerOIcon = "far fa-circle",
 playerSign = "X",
 runBot = true;
 
+/** Handles the click event on a box element.
+* Depending on the active player, it sets the innerHTML of the clicked box to the corresponding 
+* player icon,
+* Removes the active class from the current player, and adds it to the other player.
+* Sets the ID of the clicked box to the player's sign (X or O),
+* and calls the selectWinner function to check if there is a winner or a draw.
+* It disables pointer events on the clicked box and the play board, and triggers the bot function * after a random delay.t
+*/
 function clickedBox(element){
     if(players.classList.contains("player")){
         playerSign = "O";
@@ -64,6 +79,12 @@ function clickedBox(element){
     }, randomTimeDelay);
 }
 
+/** Represents the behavior of the bot's move in the game.
+* It creates an array of all empty boxes on the board and randomly selects one to make a move.
+* It checks which player is currently active and assigns the appropriate sign to the bot's move.
+* After making a move, it disables pointer events on the selected box and enables them on the 
+* play board.
+*/
 function bot(){
     let array = [];
     if(runBot){
@@ -93,14 +114,24 @@ function bot(){
     }
 }
 
+/** Takes a classname as a parameter and returns the ID value of the first element
+* that matches the selector ".box" + classname.
+*/
 function getIdVal(classname){
     return document.querySelector(".box" + classname).id;
 }
+
+//Checks if the IDs of three specified elements contain a specific player sign.
 function checkIdSign(val1, val2, val3, sign){ 
     if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
         return true;
     }
 }
+
+/** Checks the player sign in each cell of the tic-tac-toe grid and determines if there is a 
+* winner or if the match has ended in a draw.
+* If there is a winner, it displays the result box and announces the winning player.
+*/
 function selectWinner(){
     if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
         runBot = false;
